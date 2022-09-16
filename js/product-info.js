6c33ae154c218ca034ac23d9603ca9c7a22fb248
RockLord9 <diego.lopez0906@gmail.com>
2022-09-16 16:11:22 -0300

    Indiviual Entrega 3, Parte 3 Haciendo comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,25 +1,55 @@
 let categoria = localStorage.getItem('catID');
-let URL = "https://japceibal.github.io/emercado-api/cats_products/"+categoria+".json";
 let idProducto = parseInt(localStorage.getItem('idProducto'));
-let currentProductsArray = [];
-
+let URLComentarios = "https://japceibal.github.io/emercado-api/products_comments/"+idProducto+".json"
+let URL = "https://japceibal.github.io/emercado-api/products/"+idProducto+".json";
+let producto = [];
+let comentarios = [];
 document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(URL).then(function(resultObj){
         if (resultObj.status === "ok"){
-            currentProductsArray = resultObj.data.products;
-                     
+            producto = resultObj
+            
             mostrarProducto()   
+        }
+        
+    });
+    getJSONData(URLComentarios).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            comentarios = resultObj
+            console.log(comentarios);  
         }
         
     });
    
 });
 
+
+
 function mostrarProducto(){
 
-    for (producto of currentProductsArray){
-        if (producto.id == idProducto){
-        
-        }
-    }
+    let contenedorProductInfo = document.getElementById("contenedor-product-info");
+    let htmlContentToAppend = '';
+            console.log(producto) ; 
+            htmlContentToAppend += `
+            <div>
+                <h1> Articulo: ${producto.data.name} </h1> <br> <br> 
+                <p><b>Precio:</b> <br>${producto.data.cost} ${producto.data.currency} </p> <br> 
+                <p><b>Descripcion:</b> <br>${producto.data.description} <br> 
+                <p><b>Categoria:</b> <br>${producto.data.category} <br> 
+                <p><b>Vendidos:</b> <br>${producto.data.soldCount} <br> 
+                <img src = "${producto.data.images[0]}" > <br>
+            </div>`
+            contenedorProductInfo.innerHTML = htmlContentToAppend;    
+            
 }
+
+function mostrarComentarios(){
+    let contenedorComentarios = document.getElementById("comentarios");
+    let htmlContentToAppend = ``;
+        htmlContentToAppend += `<h1>PEPE</h1>`
+        
+    contenedorComentarios.innerHTML = htmlContentToAppend;
+
+    }
+    
+
